55880c89e088e79298ab16c4a12c4b3ca802ea1a
EraaNova <minhhieupham851999@gmail.com>
2023-01-01 21:55:42 +0700

    set up threejs journey portal

---
+++ src/pages/portal.tsx
@@ -0,0 +1,17 @@
+import Portal from '../components/canvas/portal';
+import type { CustomNextPage } from '../models/common';
+
+const Page: CustomNextPage = () => {
+  return <div></div>;
+};
+
+Page.Canvas = () => <Portal />;
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: 'Portal',
+      canvas: { camera: { position: [-4, 2, -4], fov: 45 } }
+    }
+  };
+}
